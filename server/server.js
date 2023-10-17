1861e7e2cc22d1621e800afde49c9eeecd627bfb
vipul <lupinworld1243@gmail.com>
2023-10-17 15:43:59 +0530

    cors apply

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+var cors = require('cors')
 const connectionDB = require("./config/dbConnection");
 const userRoute = require("./routes/userRoute");
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
@@ -10,6 +13,12 @@ dotenv.config({path: "./server/config.env"});
 const PORT = process.env.PORT || 8000;
 
 connectionDB();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 app.use("/api/student", userRoute);
 
